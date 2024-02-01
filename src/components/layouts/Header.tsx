976b8198243fc9820c0b8c7a7f361b4c962c29bd
syonosuke <syonosuke1996@outlook.jp>
2024-02-01 21:53:19 +0900

    Home画面とHeaderの調整

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,69 +1,76 @@
-"use client"
-import React, { useState } from 'react'
-
+'use client'
+import { useState } from 'react'
 
 const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false)
   const handleMenuOpen = () => {
-    setOpenMenu(!openMenu);
-  };
-
+    setOpenMenu(!openMenu)
+  }
 
   return (
     <div className="App">
-    <div className="container mx-auto px-3 bg-blue-300">
-      <header className="flex justify-between py-3 ">
-        <h1>ロゴ</h1>
+      <div className="container mx-auto px-3  bg-red-300">
+        <header className="flex justify-between py-3 ">
+          <h1>ロゴ</h1>
 
-        {/* humbergerbutton */}
-        <button
-          onClick={handleMenuOpen}
-          type="button"
-          className="z-10 space-y-2"
-        >
-          <div
-            className={
-              openMenu
-                ? "w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 transition duration-500 ease-in-out"
-                : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
-            }
-          />
-          <div
-            className={
-              openMenu
-                ? "opacity-0 transition duration-500 ease-in-out"
-                : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
-            }
-          />
-          <div
-            className={
-              openMenu
-                ? "w-8 h-0.5 bg-gray-600 -rotate-45 transition duration-500 ease-in-out"
-                : "w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out"
-            }
-          />
-        </button>
+          {/* humbergerbutton */}
+          <button
+            onClick={handleMenuOpen}
+            type="button"
+            className="z-30 space-y-2"
+          >
+            <div
+              className={
+                openMenu
+                  ? 'w-8 h-0.5 bg-gray-600 translate-y-2.5 rotate-45 transition duration-500 ease-in-out'
+                  : 'w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out'
+              }
+            />
+            <div
+              className={
+                openMenu
+                  ? 'opacity-0 transition duration-500 ease-in-out'
+                  : 'w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out'
+              }
+            />
+            <div
+              className={
+                openMenu
+                  ? 'w-8 h-0.5 bg-gray-600 -rotate-45 transition duration-500 ease-in-out'
+                  : 'w-8 h-0.5 bg-gray-600 transition duration-500 ease-in-out'
+              }
+            />
+          </button>
 
-        {/* nav */}
-        <nav
-          className={
-            openMenu
-              ? "text-left fixed bg-slate-50 right-0 top-0 w-8/12 h-screen flex flex-col justify-start pt-8 px-3 ease-linear duration-300"
-              : "fixed right-[-100%] ease-linear duration-300"
-          }
-        >
-          <ul className="mt-6">
-            <li><a href='/'>TOP</a></li>
-            <li><a href='/mylist'>マイリスト</a></li>
-            <li><a href='/mecca'>聖地の登録</a></li>
-            <li><a href='/login'>ログイン</a></li>
-            <li><a href='/register'>会員登録</a></li>
-            <li>ログアウト</li>
-          </ul>
-        </nav>
-      </header>
+          {/* nav */}
+          <nav
+            className={
+              openMenu
+                ? 'text-left fixed bg-slate-200 z-20 right-0 top-0 w-2/12  h-screen flex flex-col justify-start pt-8 px-3 ease-linear duration-300'
+                : 'fixed right-[-100%] ease-linear duration-300'
+            }
+          >
+            <ul className="mt-6 text-center ">
+              <li className="mt-2">
+                <a href="/">TOP</a>
+              </li>
+              <li className="mt-2">
+                <a href="/mylist">マイリスト</a>
+              </li>
+              <li className="mt-2">
+                <a href="/mecca">聖地の登録</a>
+              </li>
+              <li className="mt-2">
+                <a href="/login">ログイン</a>
+              </li>
+              <li className="mt-2">
+                <a href="/register">会員登録</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
     </div>
-  </div>
   )
 }
 
