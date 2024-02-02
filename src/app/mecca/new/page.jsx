094f529c794b48464eefadea6c48d6d3ead0c98d
asagohan2301 <shemomechama@yahoo.co.jp>
2024-02-02 23:25:05 +0900

    聖地登録ページの地図をモーダルで表示

--- src/app/mecca/new/page.jsx
+++ src/app/mecca/new/page.jsx
@@ -2,6 +2,7 @@
 
 import Map from '@/features/map/components/Map'
 import { getCookie } from '@/utils/cookies'
+import Link from 'next/link'
 import { useState } from 'react'
 
 export default function New() {
@@ -18,6 +19,9 @@ export default function New() {
   const [placeName, setPlaceName] = useState('')
   const [places, setPlaces] = useState([])
 
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const closeModal = () => setIsModalOpen(false)
+
   const apiUrl = process.env.NEXT_PUBLIC_API_URL
   const episodes = []
   const episodeMax = 10 // Annict から取得したい
@@ -25,7 +29,9 @@ export default function New() {
     episodes.push(i)
   }
 
-  const inputStyle = 'border-b border-gray-400 focus:outline-none'
+  const inputWrapperStyle = 'flex items-end border-b border-gray-800 pt-6'
+  const inputTitleStyle = 'mb-2 mr-5 font-semibold text-[15px]'
+  const inputStyle = 'border-gray-400 focus:outline-none h-[40px] w-[600px]'
 
   function formatScene(seconds) {
     const hours = Math.floor(seconds / 3600)
@@ -40,6 +46,8 @@ export default function New() {
     setPlaces(places)
     // 確認用
     console.log(places)
+
+    setIsModalOpen(true)
   }
 
   function handleSelectedPlace(place) {
@@ -105,12 +113,12 @@ export default function New() {
   }
 
   return (
-    <div className="flex">
+    <div className="m-auto h-[640px] w-[1100px] border border-gray-800 bg-white px-14 py-10">
       <div>
-        <h1 className="text-2xl">聖地の登録</h1>
-        <form onSubmit={postMecca}>
-          <div className="flex">
-            <p>作品名</p>
+        <h1 className="ml-5 text-[28px]">聖地の登録</h1>
+        <form onSubmit={postMecca} className="m-auto w-[800px]">
+          <div className={inputWrapperStyle}>
+            <p className={inputTitleStyle}>作品名</p>
             <input
               type="text"
               placeholder=""
@@ -120,36 +128,42 @@ export default function New() {
               }}
             />
           </div>
-          <div className="flex">
-            <p>話数</p>
-            <select
-              onChange={(e) => {
-                setEpisode(e.target.value)
-              }}
-            >
-              {episodes.map((episode) => {
-                return (
-                  <option key={episode} value={episode}>
-                    {episode}
-                  </option>
-                )
-              })}
-            </select>
+          <div className="mt-6 flex items-center gap-12">
+            <div className="flex items-center border-b border-gray-800">
+              <p className="mr-6 text-[14px] font-semibold">話数</p>
+              <select
+                onChange={(e) => {
+                  setEpisode(e.target.value)
+                }}
+                className="px-4 py-1"
+              >
+                {episodes.map((episode) => {
+                  return (
+                    <option key={episode} value={episode}>
+                      {episode}
+                    </option>
+                  )
+                })}
+              </select>
+            </div>
+            <div className="flex items-center">
+              <p className="mr-4 text-[14px] font-semibold">シーン(秒数)</p>
+              <input
+                type="range"
+                min={0}
+                max={10800}
+                onChange={(e) => {
+                  setSceneSeconds(e.target.value)
+                }}
+                className="w-[250px]"
+              />
+              <span className="ml-2 tracking-wider">
+                {formatScene(sceneSeconds)}
+              </span>
+            </div>
           </div>
-          <div className="flex">
-            <p>シーン(秒数)</p>
-            <input
-              type="range"
-              min={0}
-              max={10800}
-              onChange={(e) => {
-                setSceneSeconds(e.target.value)
-              }}
-            />
-            <span>{formatScene(sceneSeconds)}</span>
-          </div>
-          <div className="flex">
-            <p>聖地名</p>
+          <div className={inputWrapperStyle}>
+            <p className={inputTitleStyle}>聖地名</p>
             <input
               type="text"
               placeholder=""
@@ -159,37 +173,43 @@ export default function New() {
               }}
             />
           </div>
-          <div className="flex">
-            <p>場所</p>
+          <div className={inputWrapperStyle}>
+            <p className={inputTitleStyle}>場所</p>
             <input
               type="text"
-              placeholder=""
-              className={inputStyle}
+              placeholder="施設名や町の名前を入力して、検索ボタンをクリックしてください"
+              className={`${inputStyle} placeholder:text-[14px]`}
               onChange={(e) => {
                 setPlaceName(e.target.value)
               }}
               value={placeName}
             />
-            <div
-              className="cursor-pointer bg-gray-300 p-2"
-              onClick={getInfoByPlace}
-            >
-              検索する
+            <div className="mb-2.5 cursor-pointer" onClick={getInfoByPlace}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
             </div>
           </div>
-          <div className="flex">
-            <p>概要</p>
-            <input
-              type="text"
+          <div className="flex items-start border-b border-gray-800 pt-6">
+            <p className={inputTitleStyle}>概要</p>
+            <textarea
+              rows="3"
               placeholder=""
-              className={inputStyle}
+              className={`${inputStyle} h-[80px] resize-none`}
               onChange={(e) => {
                 setAbout(e.target.value)
               }}
             />
           </div>
-          <div className="flex">
-            <p>画像</p>
+          <div className={inputWrapperStyle}>
+            <p className={inputTitleStyle}>画像</p>
             <input
               type="file"
               multiple
@@ -200,12 +220,45 @@ export default function New() {
               }}
             />
           </div>
-          <button type="submit" className="bg-gray-300 p-2">
-            聖地を登録する
-          </button>
+          <div className="mt-8 flex justify-end gap-7">
+            {/* 前にいたページに戻りたいが... */}
+            <Link
+              href="/"
+              className="border border-gray-600 px-4 py-2 text-[14px] text-gray-600"
+            >
+              キャンセル
+            </Link>
+            <button
+              type="submit"
+              className="border border-gray-800 bg-[#69cefa] px-10 py-2.5 text-[14px] font-semibold"
+            >
+              登録
+            </button>
+          </div>
         </form>
       </div>
-      <Map places={places} handleSelectedPlace={handleSelectedPlace} />
+      {isModalOpen && (
+        <div className="modal-backdrop">
+          <Map
+            places={places}
+            handleSelectedPlace={handleSelectedPlace}
+            onClose={closeModal}
+          />
+          <style jsx>{`
+            .modal-backdrop {
+              position: fixed;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              background-color: rgba(0, 0, 0, 0.5);
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+          `}</style>
+        </div>
+      )}
     </div>
   )
 }
--- src/features/map/components/Map.jsx
+++ src/features/map/components/Map.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 
-export default function GoogleMap({ places, handleSelectedPlace }) {
+export default function GoogleMap({ places, handleSelectedPlace, onClose }) {
   const [selectedPlace, setSelectedPlace] = useState(null)
   const [photoUrl, setPhotoUrl] = useState(null)
 
@@ -60,27 +60,38 @@ export default function GoogleMap({ places, handleSelectedPlace }) {
   }
 
   return (
-    <div className="flex">
-      <div id="map" style={{ height: '400px', width: '400px' }} />
-      <div className="flex">
-        <div style={{ height: '300px', width: '300px', border: '1px solid' }}>
-          {selectedPlace !== null && (
-            <>
-              <p>{selectedPlace.displayName.text}</p>
-              <p>{selectedPlace.formattedAddress}</p>
-              <img src={photoUrl} alt="" />
-            </>
-          )}
-        </div>
-        <div>
-          <p>ピンをクリックすると詳細が表示されます</p>
+    <div className="flex h-[800px] w-[700px] border border-gray-800 bg-white p-6">
+      <div>
+        <div id="map" style={{ height: '400px', width: '400px' }} />
+      </div>
+      <div className="ml-6">
+        <p className="pb-10 text-[14px]">
+          ピンをクリックして場所を選んでください。
+        </p>
+        {selectedPlace !== null && (
+          <div className="mt-4">
+            <p className="text-[18px]">{selectedPlace.displayName.text}</p>
+            <p className="text-[15px]">
+              {selectedPlace.formattedAddress.split(' ')[1]}
+            </p>
+            <img src={photoUrl} alt="" />
+          </div>
+        )}
+        <div className="mt-4 flex gap-3">
           <button
-            className="bg-gray-300 p-2"
+            onClick={onClose}
+            className="border border-gray-600 px-4 py-2 text-[14px] text-gray-600"
+          >
+            もう一度検索
+          </button>
+          <button
+            className="border border-gray-800 bg-[#69cefa] px-4 py-2 text-[14px] font-semibold"
             onClick={() => {
               handleSelectedPlace(selectedPlace)
+              onClose()
             }}
           >
-            この場所を選択する
+            この場所でOK
           </button>
         </div>
       </div>
