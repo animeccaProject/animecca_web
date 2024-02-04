7254ae9dbe6c995223d2ecf4ce8dc309305c2f08
syonosuke <syonosuke1996@outlook.jp>
2024-02-04 10:40:22 +0900

    マイリストに1つリストを追加

--- src/app/mylist/page.jsx
+++ src/app/mylist/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const mylistPage = () => {
-  return (
-    <div>page</div>
-  )
+return (
+  <div class="relative m-auto w-[1100px] gap-20 border border-gray-800 bg-white p-14"><div class="absolute right-[56px] flex cursor-pointer flex-col items-center"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#ed5871" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path></svg><p class="mt-1 text-[12px]">行きたい</p></div><h2 class="text-[24px]">あの日見た花の名前を僕達はまだ知らない。</h2><p class="mb-4 text-[18px]">第2話 12:34</p><h1 class="text-[28px]">第2話で○○が働いていたカフェ</h1><div class="mb-6 flex items-center"><p class="mr-6 text-[20px]">浅草寺</p><p class="text-[16px]">東京都台東区浅草２丁目３−１</p></div><p class="text-[14px]">○○が働いていたカフェのモデルになったお店らしいです。ケーキがおいしくて、特にモンブランが有名で土日にはいつも人がたくさん来ています。○○が働いていたカフェのモデルになったお店らしいです。</p><div class="flex items-center justify-end"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#bc79f7" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"></path><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"></path></svg><p class="ml-2 text-[14px]">test さん</p></div></div>
+)
 }
 
 export default mylistPage
